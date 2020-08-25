bb9f720df899e50faefbe977bc2135206fe60f9a
Andy Wong <wongandy97@gmail.com>
2020-08-25 13:27:58 -0400

    first commit

---
+++ SidelineSwipe_coding_iOS_andywong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SidelineSwipe_coding_iOS_andywong
+//
+//  Created by Andy Wong on 8/25/20.
+//  Copyright © 2020 Andy Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
