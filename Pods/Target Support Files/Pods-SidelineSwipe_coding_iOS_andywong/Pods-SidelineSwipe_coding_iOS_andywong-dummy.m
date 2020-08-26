55b0b15cf911e8350b6b2e488c4b4944a8f1ca99
Andy Wong <wongandy97@gmail.com>
2020-08-26 10:14:14 -0400

    remove storyboard; install snpakit

---
+++ Pods/Target Support Files/Pods-SidelineSwipe_coding_iOS_andywong/Pods-SidelineSwipe_coding_iOS_andywong-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SidelineSwipe_coding_iOS_andywong : NSObject
+@end
+@implementation PodsDummy_Pods_SidelineSwipe_coding_iOS_andywong
+@end
